ef7289cf78125a805028b5306d4f687774a4853d
marcos.sobrinho <marcos.sobrinho@ati.to.gov.br>
2023-09-29 00:03:09 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
